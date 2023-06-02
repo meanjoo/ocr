ef3dafc3a1e87ef80fb8298bb62089d15e87cd79
gangmin2 <min0115b@gmail.com>
2023-06-02 13:51:47 +0900

    카메라 및 이미지 업로드 기능 추가

--- src/pages/camera.js
+++ src/pages/camera.js
@@ -1,16 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     SafeAreaView,
     StyleSheet,
-    ActivityIndicator,
+    Text,
+    Image,
 } from 'react-native'
 
-const Camera = () => {
+import { launchCamera } from 'react-native-image-picker'
+import Loading from 'pages/loading'
 
-    if (device == null) return <ActivityIndicator />
+const Camera = ({ navigation }) => {
+    const [isLoading, setLoading] = useState(false)
+    const [imageSource, setImageSource] = useState()
+    const options = {
+        storageOptions: {
+            skipBackup: true,
+            path: 'images',
+        },
+    }
+
+    const showCamera = () => {
+        launchCamera(options, (response) => {
+            if (response.error) {
+                console.error('LaunchCamera Error: ', response.error)
+            }
+            else if (response.didCancel) {
+                navigation.navigate('main')
+            }
+            else {
+                setImageSource(response.assets[0].uri)
+                setLoading(true)
+                navigation.navigate('score')
+                /**
+                 * 서버로 이미지 전송
+                 * 서버에서 파이썬 스크립트 실행
+                 * 리턴값 반환
+                 */
+            }
+        })
+    }
+
+    useEffect(() => {
+        console.log('카메라 실행')
+        showCamera()
+    }, [])
+
+    if (isLoading) {
+        return <Loading />
+    }
+    if (imageSource) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Image style={{ flex: 1, resizeMode: 'contain' }} source={{ uri: imageSource }} />
+            </SafeAreaView>
+        )
+    }
     return (
-        <SafeAreaView>
-            <Text>Camera</Text>
+        <SafeAreaView style={styles.container}>
+            <Text>Not Found</Text>
         </SafeAreaView>
     )
 }
--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     SafeAreaView,
     View,
     Text,
     Image,
-    StyleSheet
+    StyleSheet,
 } from 'react-native'
 
-const Upload = () => {
+import { launchImageLibrary } from 'react-native-image-picker'
+
+const Upload = ({ navigation }) => {
+    const [imageSource, setImageSource] = useState()
+    const options = {
+        title: 'Load Photo',
+        customButtons: [
+            { name: 'button_id_1', title: 'CustomButton 1' },
+            { name: 'button_id_2', title: 'CustomButton 2' }
+        ],
+        storageOptions: {
+            skipBackup: true,
+            path: 'images',
+        }
+    }
+
+    const showImagePicker = () => {
+        launchImageLibrary(options, (response) => {
+            if (response.error) {
+                console.error('LaunchImageLibrary Error: ', response.error)
+            }
+            else if (response.didCancel) {
+                navigation.navigate('main')
+            }
+            else {
+                setImageSource(response.assets[0].uri)
+                console.log(response.assets[0].uri)
+            }
+        })
+    }
+
+    useEffect(() => {
+        console.log('앨범 실행')
+        showImagePicker()
+    }, [])
+
+    if (imageSource) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Image style={{flex: 1, resizeMode: 'contain'}} source={{uri: imageSource}} />
+            </SafeAreaView>
+        )
+    }
     return (
-        <Text>Upload</Text>
+        <SafeAreaView style={styles.container}>
+            <Text>Not Found</Text>
+        </SafeAreaView>
     )
 }
 
